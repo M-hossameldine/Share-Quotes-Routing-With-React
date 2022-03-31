4216f1cffe9c72bf3f8608a95659fae1c9766ab7
Mohamed Hossam <m.hossameldine@gmail.com>
2022-03-31 19:47:22 +0200

    feat: integrate comments api

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,15 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from '../comments/CommentsList';
 
-const Comments = () => {
+const Comments = (props) => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
 
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedComments,
+    error,
+  } = useHttp(getAllComments, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+    comments = (
+      <div className='centered'>
+        <CommentsList comments={loadedComments} />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedComments && loadedComments.length === 0) {
+    comments = (
+      <div className='centered'>
+        <p>No comments were added yet for this Quote!</p>
+      </div>
+    );
+  }
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +65,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          onAddedComment={addedCommentHandler}
+          quoteId={quoteId}
+        />
+      )}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,9 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 import classes from './NewCommentForm.module.css';
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+  const { onAddedComment, quoteId } = props;
+
+  useEffect(() => {
+    // to notify the parent componet that the comments are updated to fetch them again
+    if (!error && status === 'completed') {
+      props.onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -11,10 +23,17 @@ const NewCommentForm = (props) => {
     // optional: Could validate here
 
     // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentData: { text: enteredText }, quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
